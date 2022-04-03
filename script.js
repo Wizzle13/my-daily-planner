811cb4497dc5017446b22cbfb0b611e5a223d0e3
CBurton <burton.chris13@gmail.com>
2022-04-03 11:09:26 -0500

    constructed Times slots.  Hardcoded was hoping for dynamic.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var startTime = 9;
-var workDay = 8;
-var i = 0;
+
 
 var displayDate = function() {
     var currentDate = moment().format('dddd[,] MMM Do YYYY');
@@ -8,11 +6,5 @@ var displayDate = function() {
     console.log(currentDate); 
 }     
 
-var dailyLayout = function() {
-    for (var i = 0; i < workDay; i++) { 
-        document.querySelector(".planner").innerHTML = "<h3>" + startTime + "</h3><br/>";
-        console.log(startTime);
-    }
-}
+
 displayDate();
-dailyLayout();
