3f6876be4ee442025c7ad84f666ceb05d4a7dd2d
CBurton <burton.chris13@gmail.com>
2022-04-02 19:33:58 -0500

    completed the date, stalled on planner layout

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-var displayDate =function() {
-    var currentDate=moment().format('dddd[,] MMM Do YYYY');
-   // formEl.querySelector("#currentDay").appendChild = currentDate;
+var startTime = 9;
+var workDay = 8;
+var i = 0;
+
+var displayDate = function() {
+    var currentDate = moment().format('dddd[,] MMM Do YYYY');
+    document.querySelector("#currentDay").innerHTML = currentDate;
     console.log(currentDate); 
-}        
+}     
 
-var formEl = document.querySelector("#currentDay");
-
+var dailyLayout = function() {
+    for (var i = 0; i < workDay; i++) { 
+        document.querySelector(".planner").innerHTML = "<h3>" + startTime + "</h3><br/>";
+        console.log(startTime);
+    }
+}
 displayDate();
+dailyLayout();
